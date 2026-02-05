956336270a0f9d5e4011102379fbb88b0590a5a4
Your GitHub Username <your_email@example.com>
2026-02-05 19:39:03 +0300

    more changes

---
+++ config/config.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2/log"
+	"github.com/joho/godotenv"
+)
+func LoadEnv()  {
+  if err := godotenv.Load();err != nil{
+   log.Fatal("no dotenv file  found ")
+  }
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.25.6
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.11
+	github.com/joho/godotenv v1.5.1
 	gorm.io/gorm v1.31.1
 )
 
