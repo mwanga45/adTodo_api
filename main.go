2b0186bdf1c13d0a4667389ca1fbc35889caefa5
Your GitHub Username <your_email@example.com>
2026-02-05 19:01:10 +0300

    more changes

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+	
+
+	log.Fatal(app.Listen(":3000"))
+}
