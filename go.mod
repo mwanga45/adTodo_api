66c877e8b5a64708d8ccf26cd4c8b43775d9f12a
mwanga45 <issamwanga02@gmail.com>
2026-02-06 15:15:24 +0300

    more changes

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.25.6
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.11
+	github.com/golang-jwt/jwt/v5 v5.3.1
 	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.31.0
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.31.1
 )
@@ -26,7 +28,6 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.33.0 // indirect
---
+++ utils/generatetoken.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"time"
+	"todo_api_backend/model"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/joho/godotenv"
+)
+
+func Generatetoken(user *model.User)(string,error){
+	// generate an token
+	secrete_key := godotenv.Load("SECRETE_KEY")
+	claims := jwt.MapClaims{
+		"username":user.Username,
+		"userId":user.ID,
+		"exp":time.Now().Add(time.Hour *24).Unix(),
+	}
+	method := jwt.SigningMethodES256
+
+	token , err := jwt.NewWithClaims(method,claims).SignedString(secrete_key)
+
+	if err != nil{
+		// log.Fatal(err)
+		return  "", err
+	}
+	return  token, nil
+
+
+
+}
